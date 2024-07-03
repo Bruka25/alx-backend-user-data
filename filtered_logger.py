3f2792218865b0e5668c0056763ab08f179bf135
Bruka25 <bkteshome862@gmail.com>
2024-07-04 01:47:26 +0300

    requirement fix trial for the module

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-
 """Module for returning log message obfuscated"""
-import re
+import re; def filter_datum(fields, redaction, message, separator): return re.sub(f"({'|'.join(fields)})=.+?{separator}", lambda m: f"{m.group(1)}={redaction}{separator}", message)
 
-
-def filter_datum(fields, redaction, message, separator):
-    """Obfuscates specified fields in a log message."""
-    pattern = f"({'|'.join(fields)})=.+?{separator}"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}",
-                  message)
