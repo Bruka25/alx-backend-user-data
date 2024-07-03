55fcd464ef63f7276feab7c762c2df5d3df816c5
Bruka25 <bkteshome862@gmail.com>
2024-07-04 01:36:43 +0300

    module for returning obfuscated log message

--- filtered_logger.py
+++ filtered_logger.py
@@ -5,15 +5,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates specified fields in a log message.
-
-    Args:
-        fields (list): Fields to obfuscate.
-        redaction (str): Replacement string for obfuscation.
-        message (str): Log message to be obfuscated.
-        separator (str): Character that separates fields in the log message.
-    """
+    """Obfuscates specified fields in a log message."""
     pattern = f"({'|'.join(fields)})=.+?{separator}"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}",
                   message)
