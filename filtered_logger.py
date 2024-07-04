5473df1f702112d72d0340067e6e1eb1b91bbdec
Bruka25 <bkteshome862@gmail.com>
2024-07-04 21:49:00 +0300

    another trial for the third task requirement fix

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
 """Module for returning log message obfuscated"""
-
 import re
 from typing import List
 
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-
-
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
-    """Filters a log line.
-    """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscates specified fields in a log message"""
+    pattern = f"({'|'.join(fields)})=.+?{separator}"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}",
+                  message)
