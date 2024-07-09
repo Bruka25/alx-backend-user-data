113a6443e9eb78ca818478587b5c2d4652074c14
Bruka25 <bkteshome862@gmail.com>
2024-07-09 22:30:19 +0000

    a module that returns the decoded value of base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,3 +23,20 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')  # Return captured token part
         return None  # Return None if not a valid Basic Auth header
+
+    def decode_base64_authorization_header(
+           self,
+           base64_authorization_header: str) -> str:
+        """Decodes a base64-encoded authorization header.
+        Args:
+          - base64_authorization_header: The base64-encoded header string.
+        Return:
+          - Decoded header as a UTF-8 string or None if decoding fails.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(base64_authorization_header,
+                                       validate=True)
+                return res.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None  # Return None if decoding fails
