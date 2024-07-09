ab46fa263aa02c3b506f953b254e94047aaf2c8e
Bruka25 <bkteshome862@gmail.com>
2024-07-09 23:15:44 +0000

    basic authentication requirement fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -56,11 +61,4 @@ def verify_user():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth_type = getenv('AUTH_TYPE', 'auth')
-    if auth_type == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    if auth_type == 'basic_auth':
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
     app.run(host=host, port=port)
