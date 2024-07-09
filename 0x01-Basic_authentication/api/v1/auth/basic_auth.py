8e2851d27d2c252d50d878d3fc9c0662668da1a5
Bruka25 <bkteshome862@gmail.com>
2024-07-09 22:56:29 +0000

    complete task of basic authentication from 9 to 12

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ from .auth import Auth
 import re
 import base64
 import binascii
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -42,3 +44,59 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None  # Return None if decoding fails
+
+    def extract_user_credentials(
+           self,
+           decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """Extracts user credentials from a base64-decoded auth header.
+        Args:
+          - decoded_base64_authorization_header: The decoded header string.
+        Return:
+          - A tuple (user, password) or (None, None) if extraction fails.
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
+
+    def user_object_from_credentials(
+             self,
+             user_email: str,
+             user_pwd: str) -> TypeVar('User'):
+        """Retrieves a user based on authentication credentials.
+         Args:
+           - user_email: The user's email.
+           - user_pwd: The user's password.
+         Return:
+           - A User object or None if authentication fails.
+        """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+         Args:
+           - request: The request object.
+         Return:
+           - A User object or None if authentication fails.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
