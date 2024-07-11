5e84d273e174bee3549a15a2e1f768757b19572b
Bruka25 <bkteshome862@gmail.com>
2024-07-11 11:40:22 -0700

    update the app module for getting a user based on its session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,7 @@
 """Module for session authentication"""
 import uuid
 from api.v1.auth.auth import Auth
-
+from models.user import User
 
 class SessionAuth(Auth):
     """Session authentication class."""
@@ -28,3 +28,11 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value."""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id:
+            return User.get(user_id)
+        return None
