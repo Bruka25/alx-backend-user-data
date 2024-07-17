b53bc0a13fd19974a53cc4f9f46691a8ad6a588e
Bruka25 <bkteshome862@gmail.com>
2024-07-17 21:47:47 +0000

    little adjustment to the user module

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -24,8 +24,8 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    email: str = Column(String(250), nullable=False)
-    hashed_password: str = Column(String(250), nullable=False)
+    id: = Column(Integer, primary_key=True, autoincrement=True)
+    email: = Column(String(250), nullable=False)
+    hashed_password: = Column(String(250), nullable=False)
     session_id: = Column(String(250), nullable=True)
     reset_token: = Column(String(250), nullable=True)
