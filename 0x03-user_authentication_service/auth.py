e019132f95568655acef7c4578c1afa3c67bc333
Bruka25 <bkteshome862@gmail.com>
2024-07-18 09:30:39 -0700

    update the auth module to find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,3 +89,20 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user by session ID
+
+        Args:
+            session_id (str): The session ID of the user.
+
+        Returns:
+            User: The User object if found, None otherwise.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
