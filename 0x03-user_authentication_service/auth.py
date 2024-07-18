ae6527a657cf1d8fd0ec18c63c9f7b67a48176a7
Bruka25 <bkteshome862@gmail.com>
2024-07-18 10:25:44 -0700

    improve the auth module for resseting passsword token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -122,3 +122,27 @@ class Auth:
             self._db._session.commit()
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a password reset token for a user.
+
+        Args:
+            email (str): Email address of the user for whom the token is
+                         generated.
+
+        Returns:
+            str: The generated password reset token.
+
+        Raises:
+            ValueError: If no user with the provided email address is found.
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
