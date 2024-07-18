c094897cdef80327b3d903136495f2d7a880c969
Bruka25 <bkteshome862@gmail.com>
2024-07-18 12:33:46 +0000

    basic flask setup pep8 fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome() -> str:
     """GET route to return a welcome message
@@ -13,6 +14,7 @@ def welcome() -> str:
         str: JSON payload with a welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
