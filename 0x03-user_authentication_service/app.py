682faaf13153da8d378818c29faaf381739a9de5
Bruka25 <bkteshome862@gmail.com>
2024-07-18 09:07:19 -0700

    update app module by responding to the post sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app with user registration
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -34,6 +34,22 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """POST route to login a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(
+        jsonify({"email": email, "message": "logged in"})
+    )
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
